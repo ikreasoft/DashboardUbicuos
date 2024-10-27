950caaecc02b6e992efc389b903fe15f36f9486a
chunche95 <paeste95.pb@gmail.com>
2024-10-27 15:47:48 +0100

    Mod. Adjust the astro config file

--- justmovedoc/astro.config.mjs
+++ justmovedoc/astro.config.mjs
@@ -6,16 +6,23 @@ import { loadEnv } from 'vite';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import expressiveCode from 'astro-expressive-code';
+import remarkMermaid from 'remark-mermaidjs'
 // import basicSsl from '@vitejs/plugin-basic-ssl';
 
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        // Applied to .md and .mdx files
+        remarkPlugins: [remarkMermaid],
+    },
     integrations: [starlight({
         title: 'Just Move Doc -  Dashboard',
         logo: {
-            light: './src/assets/img/logomami.webp',
-            dark: './src/assets/img/logomami-dark.webp'
+            light: './src/assets/img/logo/logomami.webp',
+            dark: './src/assets/img/logo/logomami-dark.webp'
         },
         social: {
             github: 'https://github.com/ikreasoft/DashboardUbicuos/tree/docs',
@@ -26,17 +33,25 @@ export default defineConfig({
         sidebar: [            
             {
                 label: 'Introducción',
-                autogenerate: { directory: 'pages' },                
+                autogenerate: { directory: 'pages/index' },                
             },
             {
                 label: 'Guias',
                 items: [
                     // Each item here is one entry in the navigation menu.                    
-                    { label: 'Planning',slug: 'guides/planning'},
-                    { label: 'Estado del arte',slug: 'guides/estadodelarte'},
-                    { label: 'Guia de configuración', slug: 'guides/configuracionproyecto' },
+                    { label: 'Planning',slug: 'guides/1planning'},
+                    { label: 'Estado del arte',slug: 'guides/2estadodelarte'},
+                    { label: 'Guia de configuración', slug: 'guides/3configuracionproyecto' },
+                    { label: 'Casos de uso', slug: 'guides/4diagramadecasosdeuso' },
+                    { label: 'Diagrama proyecto', slug: 'guides/5diagramadoproyecto' },
+                    { label: 'Estructura proyecto', slug: 'guides/6estructuraproyecto' },
+                    { label: 'Infraestructura física Living Lab', slug: 'guides/7infraestructuradellivinglab' },
                     
                 ]
+            },
+            {
+                label: 'Simulación de datos con Python',
+                autogenerate: { directory: 'blog' },                
             },
             {
                 label: 'Referencias',
@@ -47,7 +62,7 @@ export default defineConfig({
                 autogenerate: { directory: 'team' },                
             },
         ],
-        }), tailwind(), mdx(), sitemap()],
+        }), expressiveCode(), tailwind(), mdx(), sitemap(), react()],
         i18n: {
             defaultLocale: "es",
             locales: ["es", "en", "fr", "pt-br"],
