0fcf5d1abf6950b079c6426af8e05b21e75a1692
Alfonso Sebastián Simorte Ramos <117114417+loleandote22@users.noreply.github.com>
2025-01-17 23:59:47 +0100

    api fuera

--- API/models/Audit.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-var AuditShema = new Schema({
-    user: {type: String, required: true},
-    date: {type: Date, default: Date.now},
-    action: {type: String, required: true}
-});
-module.exports = mongoose.model("Audit", AuditShema);
--- API/models/Role.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-var RoleSchema = new Schema({
-    name: String,
-    permissions: [{type: Schema.Types.ObjectId, ref: "Permision"}]
-});
-module.exports = mongoose.model("Role", RoleSchema);
