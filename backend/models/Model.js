4135ac1d4dd2bd264bcbfb3105e2cd5d1ac0404e
kevin15sandoval <96436994+kevin15sandoval@users.noreply.github.com>
2025-01-17 23:42:54 +0100

    cambios modles

--- backend/models/Model.js
+++ backend/models/Model.js
@@ -1,20 +1,14 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var CameraSchema = new Schema({
+var ModelSchema = new Schema({
   name: String,
-  type: String,
   location: String,
   protocol: String,
-  ipAddress: String,
-  model: String,
-  macAddress: String,
   firmwareVersion: String,
-  lastConnection: Date,
-  isActive: Boolean,
   resolution: String,
-  streamUrl: String,
-  isRecording: Boolean,
+  streamUrl: String
+  // isRecording: Boolean,
   // configuration: Object,
   // alertRule: Object
 });
-module.exports = mongoose.model("Camera", CameraSchema, this.collection="devices");
+module.exports = mongoose.model("Model", ModelSchema);
